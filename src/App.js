f6115d84066bce85e9fbb08b01e67b48c1b332f4
Juan Almonte <almonteestrella@gmail.com>
2019-10-17 19:12:40 -0400

    deploy issue

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import cards from "./cards.json";
-import "./App.css";
 
 class App extends Component {
   // Setting this.state.cards to the cards json array
@@ -15,7 +14,7 @@ class App extends Component {
 
   gameOver = () => {
     if (this.state.score > this.state.highscore) {
-      this.setState({highscore: this.state.score}, function() {
+      this.setState({ highscore: this.state.score }, function() {
         console.log(this.state.highscore);
       });
     }
@@ -23,31 +22,33 @@ class App extends Component {
       card.count = 0;
     });
     alert(`Game Over :( \nscore: ${this.state.score}`);
-    this.setState({score: 0});
+    this.setState({ score: 0 });
     return true;
-  }
+  };
 
   clickCount = id => {
     this.state.cards.find((o, i) => {
       if (o.id === id) {
-        if(cards[i].count === 0){
+        if (cards[i].count === 0) {
           cards[i].count = cards[i].count + 1;
-          this.setState({score : this.state.score + 1}, function(){
+          this.setState({ score: this.state.score + 1 }, function() {
             console.log(this.state.score);
           });
-          this.state.cards.sort(() => Math.random() - 0.5)
-          return true; 
+          this.state.cards.sort(() => Math.random() - 0.5);
+          return true;
         } else {
           this.gameOver();
         }
       }
     });
-  }
+  };
   // Map over this.state.cards and render a cardCard component for each card object
   render() {
     return (
       <Wrapper>
-        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        <Header score={this.state.score} highscore={this.state.highscore}>
+          Clicky Game
+        </Header>
         {this.state.cards.map(card => (
           <Card
             clickCount={this.clickCount}
